a50a9b0a74a0d6a8c50fe74d17acd36af1634a39
Serena 𖦹 <serena.wang1906@gmail.com>
2026-07-17 16:21:09 -0400

    Create apply_discount.py

---
+++ apply_discount.py
@@ -0,0 +1,15 @@
+def apply_discount(price, discount):
+    if not isinstance(price, (int, float)):
+        return 'The price should be a number'
+    
+    if not isinstance(discount, (int, float)):
+        return 'The discount should be a number'
+    
+    if price <= 0:
+        return 'The price should be greater than 0'
+    
+    if discount < 0 or discount > 100:
+        return 'The discount should be between 0 and 100'
+    
+    apply_discount = price - (price * (discount/100))
+    return apply_discount
